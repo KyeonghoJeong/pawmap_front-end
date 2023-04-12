566406686950f4149935b3cf0da22145326017ed
KyeonghoJeong <kyeongho.jeong.engineer@gmail.com>
2023-04-12 23:46:39 +0900

    Header

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark custom-navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style="font-size: 25px; font-weight: bold; margin-bottom: 6px">pawmap</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">지도</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">게시판</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">갤러리</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">로그아웃</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">마이페이지</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default{
+    
+}
+</script>
+
+<style>
+.custom-navbar {
+  background-color: #fd7e14;
+}
+.navbar-collapse{
+    font-size:14px;
+}
+.nav-link:hover {
+    font-weight: bold;
+}
+.custom-navbar {
+  background-color: #fd7e14;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
