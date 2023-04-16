7740729a239f58c895becf26c546e5398ce4ef3c
KyeonghoJeong <kyeongho.jeong.engineer@gmail.com>
2023-04-16 14:25:23 +0900

    Added Sign Up

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -4,14 +4,13 @@
             <div class="container-signin">
                 <h1 class="h3 mb-3 font-weight-normal">로그인</h1><br>
                 <p>서비스 이용을 위해 로그인해주세요.</p><br>
-                <label for="inputEmail" class="sr-only">아이디</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="아이디 입력" required autofocus><br>
-                <label for="inputPassword" class="sr-only">비밀번호</label>
+                <label for="inputId" class="sr-only custom-labels">아이디</label>
+                <input type="text" pattern="[A-Za-z0-9]+" id="inputId" class="form-control" placeholder="아이디 입력" required autofocus><br>
+                <label for="inputPassword" class="sr-only custom-labels">비밀번호</label>
                 <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호 입력" required><br>
-                <div class="checkbox mb-3">
-                    <label>
-                    <input type="checkbox" value="remember-me"> 아이디 저장
-                    </label>
+                <div class="checkbox mb-3 custom-checkbox-saveid">
+                    <label style="margin-right: auto;"><input type="checkbox" value="remember-me"> 아이디 저장</label>
+                    <router-link to="/signup" class="custom-router-link-signup">회원가입</router-link>
                 </div>
                 <button class="btn btn-lg btn-primary btn-block" type="submit" style="width:100%">로그인</button>
             </div>
@@ -36,6 +35,19 @@
     padding-left: 5%;
     padding-right: 5%;
     font-size: 0.8rem;
+}
+.custom-checkbox-saveid{
+    display: flex;
+    align-items: center;
+}
+.custom-router-link-signup{
+    margin-left: auto;
+    color: black;
+    font-weight: bold;
+    text-decoration: none;
+}
+.custom-labels{
+    font-weight: bold;
 }
 
 @media screen and (max-width: 992px){
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,3 +1,67 @@
 <template>
-    <h1>This is Sign Up page</h1>
+    <body>
+        <form class="form-signin custom-signin">
+            <div class="container-signin">
+                <h1 class="h3 mb-3 font-weight-normal">회원정보 입력</h1><br>
+                <p>회원가입을 위해 정보를 입력해주세요.</p><br>
+                <label for="inputId" class="sr-only custom-labels">아이디</label>
+                <input type="text" pattern="[A-Za-z0-9]+" id="inputId" class="form-control" placeholder="아이디 입력" required autofocus><br>
+                <label for="inputNickName" class="sr-only custom-labels">닉네임</label>
+                <input type="text" pattern="[A-Za-z0-9]+" id="inputNickName" class="form-control" placeholder="닉네임 입력" required autofocus><br>
+                <label for="inputPassword" class="sr-only custom-labels">비밀번호</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호 입력" required><br>
+                <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호 확인" required><br><br>
+                <button class="btn btn-lg btn-primary btn-block" type="submit" style="width:100%">확인</button>
+            </div>
+        </form>
+  </body>
 </template>
+
+<style>
+.custom-signin{
+    margin-top: 4.4%;
+    margin-bottom: 4.4%;
+    margin-left: 33%;
+}
+.container-signin{
+    width: 50%;
+    height: 50%;
+    border-style: solid;
+    border-color: rgb(189, 189, 189);
+    border-width: 1px;
+    padding-top: 5%;
+    padding-bottom: 5%;
+    padding-left: 5%;
+    padding-right: 5%;
+    font-size: 0.8rem;
+}
+.custom-checkbox-saveid{
+    display: flex;
+    align-items: center;
+}
+.custom-router-link-signup{
+    margin-left: auto;
+    color: black;
+    font-weight: bold;
+    text-decoration: none;
+}
+
+@media screen and (max-width: 992px){
+    .custom-signin{
+        margin-left: 28%;
+    }
+    .container-signin{
+        width: 60%;
+        height: 60%;
+    }
+}
+@media screen and (max-width: 576px){
+    .custom-signin{
+        margin-left: 16%;
+    }
+    .container-signin{
+        width: 80%;
+        height: 80%;
+    }
+}
+</style>
